303baf5b0a21229a1b5716ad4e3bdbfc7ff31169
Furkan Can Baytemur <41757315+Kaaleyah@users.noreply.github.com>
2023-01-10 13:53:12 +0300

    Initial Commit

---
+++ Podify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Podify
+//
+//  Created by Furkan Can Baytemur on 10.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
